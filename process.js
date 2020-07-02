3c5e15f0b0b962c6dd128c13d5ad82ce2e986d27
CosmiCloud <tybranha@gmail.com>
2020-07-02 08:50:09 -0400

    Add existing file

--- process.js
+++ process.js
@@ -1,13 +1,19 @@
 const { exec } = require("child_process");
 const fs = require('fs');
+const dateFormat = require('dateformat');
 require('dotenv').config();
 
 var container = process.env.CONTAINER;
 var logPath = '/var/lib/docker/containers/'+container+'/'+container+'-json.log';
 var date = new Date();
-var date = date.getMonth();
-var master = 'sudo mkdir ~/NodeArchive && sudo cp '+logPath+' ~/NodeArchive/nodeLogs && sudo tar -czf ~/NodeArchive/nodeLogs_month'+date+'.tar ~/NodeArchive/nodeLogs && sudo rm -rf ~/NodeArchive/nodeLogs'
+var date = dateFormat(new Date(), "yyyy-mm-dd-h:MM:ss");
+var master = 'sudo mkdir -p ~/NodeArchive && sudo cp '+logPath+' ~/NodeArchive/nodeLogs && sudo tar -czf ~/NodeArchive/nodeLogs_month'+date+'.tar ~/NodeArchive/nodeLogs && sudo rm -rf ~/NodeArchive/nodeLogs'
 
-exec(master, (error, stdout, stderr) => {});
-
-fs.truncate(logPath,0,function(){console.log('Logs archived')});
+exec(master, (error, stdout, stderr) => {
+  if (error){
+    console.error(error);
+  }
+  else{
+    fs.truncate(logPath,0,function(){console.log('Logs archived')});
+  }
+});
