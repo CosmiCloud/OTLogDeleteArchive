0856cfab05663679d7d0227f56e11302bcf0aaba
CosmiCloud <42195091+CosmiCloud@users.noreply.github.com>
2020-07-04 23:38:41 -0400

    Update process.js

--- process.js
+++ process.js
@@ -7,7 +7,7 @@ var container = process.env.CONTAINER;
 var logPath = '/var/lib/docker/containers/'+container+'/'+container+'-json.log';
 var date = new Date();
 var date = dateFormat(new Date(), "yyyy-mm-dd-h:MM:ss");
-var master = 'sudo mkdir -p ~/NodeArchive && sudo cp '+logPath+' ~/NodeArchive/nodeLogs && sudo tar -czf ~/NodeArchive/nodeLogs_month'+date+'.tar ~/NodeArchive/nodeLogs && sudo rm -rf ~/NodeArchive/nodeLogs'
+var master = 'sudo mkdir -p ~/NodeArchive && sudo cp '+logPath+' ~/NodeArchive/nodeLogs && sudo tar -czf ~/NodeArchive/nodeLogs_'+date+'.tar ~/NodeArchive/nodeLogs && sudo rm -rf ~/NodeArchive/nodeLogs'
 
 exec(master, (error, stdout, stderr) => {
   if (error){
